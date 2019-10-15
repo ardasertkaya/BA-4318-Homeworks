d8cd229cd1bf83fad75d50fdd468a268c7156e7d
ardasertkaya <56051183+ardasertkaya@users.noreply.github.com>
2019-10-15 20:20:00 +0300

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,11 @@
+name = input("Please enter your name ")
+print("Hello, ", name)
+age = int (   input("Please enter your age ")  )
+if age < 1 :
+    print ("You are an infant")
+elif age < 10 :
+    print ("You are an child")
+elif age < 20 :
+    print ("You are an adolescent")
+else :
+    print ("You are an adult")
